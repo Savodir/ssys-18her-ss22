0de2b622fc039912361f51641f7dfe01e486ba82
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-08-08 22:47:44 +0200

    9DOF stick code + library

--- src/arduino/DummyRobot/DummyRobot.ino
+++ src/arduino/DummyRobot/DummyRobot.ino
@@ -1,25 +1,47 @@
 #include <Servo.h>
 #include <NewPing.h>
-
+#include <LSM9DS1_Registers.h>
+#include <LSM9DS1_Types.h>
+#include <SparkFunLSM9DS1.h>
+#include <Wire.h>
+#include <SPI.h>
 NewPing sensor1(7, 6, 300);
 NewPing sensor2(5, 4, 300);
 
 String xval = "";
 String yval = "";
 String yvaltemp = "";
-int x = 0;
+//If currentpos < 180 == 0 + (currentheading / 2)
+//Else 360 - (CURRENTHEADING / 2) = Pos
+int x = 0; 
 int y = 0;
 float distance1 = 0, distance2 = 0;
+int corner;
+int pos = 0;
+int currentHeading;
 int sensorX = 230;
 int sensorY = 256;
+double drift;
 int wrongvalue = 5;
 bool commandGiven = false;
+long currentMillis;
+long prevMillis;
+double pastTime;
 int ledWrong = 12;
 int ledRight = 11;
-int pos = 0;
+
 char inbyte = "";
 Servo myservo;
 String receivedData;
+LSM9DS1 imu;
+#define LSM9DS1_M 0x1E
+#define LSM9DS1_AG  0x6B
+
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+
+#define DECLINATION 0
+
 void setup() {
   Serial.begin(9600);
   myservo.attach(8);
@@ -27,16 +49,40 @@ void setup() {
   pinMode(ledRight, OUTPUT);
   digitalWrite(ledWrong, HIGH);
   digitalWrite(ledRight, LOW);
+  corner = 0;
+  drift = 1.40;
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+
+  drift = 1.40;
+  currentHeading = 0;
+  currentMillis = 0;
+  prevMillis = 0;
+  pastTime = 0;
+
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+    //while (1)   Serial.println("Hello");
+  }
+  recalibrate();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   readDistance();
-  delay(500);
+  readSensors();
+  delay(100);
   if(sensorX >= x - wrongvalue && sensorY >= y - wrongvalue && sensorX <= x + wrongvalue && sensorY <= y + wrongvalue) {
     digitalWrite(ledWrong, LOW);
     digitalWrite(ledRight, HIGH);
-    }
+    } 
 if (Serial.available() > 0)
   {
     inbyte = Serial.read();
@@ -53,8 +99,7 @@ if (Serial.available() > 0)
     break;
   case 'x':
   //CalibrationChar, tijdelijke testchar
-    pos = pos + 10;
-    myservo.write(pos);
+    recalibrate();
     break;
    default:
    receivedData = receivedData + inbyte;
@@ -91,7 +136,58 @@ void readDistance() {
   Serial.println(distance1);
   Serial.print("Distance2= ");
   Serial.println(distance2);
+}
+void readSensors() {
+  //Reads out the Gyro sensor for heading calculations
+  if ( imu.gyroAvailable() )
+  {
+    // To read from the gyroscope,  first call the
+    // readGyro() function. When it exits, it'll update the
+    // gx, gy, and gz variables with the most current data.
+    imu.readGyro();
   }
+
+  currentMillis = millis();
+
+  pastTime = (currentMillis - prevMillis) * 0.001;
+  if((imu.calcGyro(imu.gz)- drift) > 0.5 || (imu.calcGyro(imu.gz)- drift) < -0.5)
+  {
+    currentHeading += (imu.calcGyro(imu.gz) - drift) * pastTime * 2; 
+  }
+  if(currentHeading < 0){
+    currentHeading = currentHeading + 360;
+  }
+  if(currentHeading >= 360){
+    currentHeading = currentHeading - 360;
+  }
+  prevMillis = currentMillis;
+    Serial.println(currentHeading);
+
+}
+void recalibrate() {
+  //Calibrates the sensors
+  //Calibrate Gyro
+  float tempDrift = 0;
+  delay(600);
+  for(int i = 0; i < 10; i++){
+    delay(100);
+    if ( imu.gyroAvailable() )
+    {
+      // To read from the gyroscope,  first call the
+      // readGyro() function. When it exits, it'll update the
+      // gx, gy, and gz variables with the most current data.
+      imu.readGyro();
+    }
+    tempDrift += imu.calcGyro(imu.gz);
+  }
+
+  drift = tempDrift/10;
+  
+  //And normalize distance sensor readings
+  currentHeading = 0;
+}
+
+  
 String getValue(String data, char separator, int index)
 {
     int found = 0;
